139ece07062207044d4acb4bc13f246debd8ebda
NoamZa <Noamza96@gmail.com>
2023-09-19 08:43:40 +0300

    Fixed Labels working with live time

--- PredictionsUI/src/JavaFx/SubComponents/header/HeaderController.java
+++ PredictionsUI/src/JavaFx/SubComponents/header/HeaderController.java
@@ -1,9 +1,11 @@
 package JavaFx.SubComponents.header;
 
 import JavaFx.App.PredictionsAppController;
+import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -11,6 +13,10 @@ import javafx.stage.FileChooser;
 import org.omg.CosNaming.BindingIterator;
 
 import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class HeaderController {
     private PredictionsAppController mainController;
@@ -26,6 +32,22 @@ public class HeaderController {
     private Label FinishedSimLbl;
 
     @FXML
+    public void initialize() {
+    Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+        @Override
+        public void run() {
+            Platform.runLater(() -> {
+                ExecutorService myService = mainController.getThreadManager().getThreadExecutor();
+                QueueSizeLbl.setText(String.valueOf(((ThreadPoolExecutor)myService).getQueue().size()));
+                RunningSimLbl.setText(String.valueOf(((ThreadPoolExecutor)myService).getActiveCount()));
+                FinishedSimLbl.setText(String.valueOf((int)((ThreadPoolExecutor)myService).getCompletedTaskCount()));
+            });
+        }
+    }, 0, 1000);
+    }
+
+    @FXML
     void loadXmlFileButtonPressedHandler(ActionEvent event) {
         FileChooser fileChooserBtn = new FileChooser();
         fileChooserBtn.setTitle("XML File Chooser");
@@ -36,6 +58,7 @@ public class HeaderController {
         }
         String absolutePath = selectedFile.getAbsolutePath();
         mainController.readWorldData(absolutePath);
+
     }
 
     public void setMainController(PredictionsAppController mainController) {
@@ -43,9 +66,9 @@ public class HeaderController {
     }
     public void bindHeaderToFullApp(){
         xmlPathTextField.textProperty().bind(mainController.selectedFileProperty());
-        QueueSizeLbl.textProperty().bind(Bindings.format("%d",mainController.getQueueSize()));
-        RunningSimLbl.textProperty().bind(Bindings.format("%d",mainController.getRunningSimLabel()));
-        FinishedSimLbl.textProperty().bind(Bindings.format("%d",mainController.getFinishedSimLabel()));
+//        QueueSizeLbl.textProperty().bind(Bindings.format("%d",mainController.getQueueSize()));
+//        RunningSimLbl.textProperty().bind(Bindings.format("%d",mainController.getRunningSimLabel()));
+//        FinishedSimLbl.textProperty().bind(Bindings.format("%d",mainController.getFinishedSimLabel()));
     }
 }
 
